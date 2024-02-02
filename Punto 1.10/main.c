d5e90904816c7834303172c019c6f289fd2d9824
Ricardojfh22 <157148374+Ricardojfh22@users.noreply.github.com>
2024-02-02 09:59:09 -0400

    Add files via upload

---
+++ Punto 1.10/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Aplicación de operadores. */
+
+void main(void) {
+
+  int x, y, k, j = 7;
+
+  x = 9 + 32;
+  y = 186 + 42;
+
+  printf("\nEl valor de x es: %d", x);
+
+  ++x;
+
+  printf("\nEl valor de y es: %d", y);
+
+  j = 5 + (k = 3);
+
+  printf("\nEl valor de j es: %d", j);
+
+}
